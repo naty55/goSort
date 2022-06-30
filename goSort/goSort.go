0239b559cfc93b4ac823c0f20ffd950762a4f152
Naty Mina <64107292+naty55@users.noreply.github.com>
2022-07-01 00:32:47 +0300

    adding insertion sort

--- goSort/goSort.go
+++ goSort/goSort.go
@@ -1,1 +1,17 @@
-package gosort
+package goSort
+
+func InsertionSort(matrix [][3]int32, isBigger func([3]int32, [3]int32) bool) {
+	for key := 1; key < len(matrix); key++ {
+		current := matrix[key]
+		var idx int
+		for idx = key - 1; idx >= 0; idx-- {
+			if isBigger(matrix[idx], current) {
+				matrix[idx+1] = matrix[idx]
+			} else {
+				break
+			}
+		}
+		matrix[idx+1] = current
+
+	}
+}
