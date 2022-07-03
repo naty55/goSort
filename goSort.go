7a6fb5e7f7c7360dd7b7db7dead7fd8de18f5ce3
Naty Mina <64107292+naty55@users.noreply.github.com>
2022-07-04 02:13:11 +0300

    Added merge sort

--- goSort.go
+++ goSort.go
@@ -15,3 +15,44 @@ func InsertionSort(matrix [][3]int32, isBigger func([3]int32, [3]int32) bool) {
 
 	}
 }
+func MergeSort(matrix [][3]int32, isBigger func([3]int32, [3]int32) bool) {
+	mergeSort(matrix, 0, len(matrix), isBigger)
+}
+
+func mergeSort(matrix [][3]int32, start int, end int, isBigger func([3]int32, [3]int32) bool) {
+	if end <= start {
+		return
+	}
+	p := int((end + start) / 2)
+	mergeSort(matrix, start, p, isBigger)
+	mergeSort(matrix, p+1, end, isBigger)
+	merge(matrix, start, p, end, isBigger)
+
+}
+
+func merge(matrix [][3]int32, start int, middle int, end int, isBigger func([3]int32, [3]int32) bool) {
+	matrix1 := append(make([][3]int32, 0), matrix[start:middle]...)
+	matrix2 := append(make([][3]int32, 0), matrix[middle:end]...)
+	i := 0
+	j := 0
+	var key int
+	for key = start; i < len(matrix1) && j < len(matrix2); key++ {
+		if isBigger(matrix1[i], matrix2[j]) {
+			matrix[key] = matrix2[j]
+			j++
+		} else {
+			matrix[key] = matrix1[i]
+			i++
+		}
+	}
+
+	for ; i < len(matrix1); i++ {
+		matrix[key] = matrix1[i]
+		key++
+	}
+
+	for ; j < len(matrix2); j++ {
+		matrix[key] = matrix2[j]
+		key++
+	}
+}
